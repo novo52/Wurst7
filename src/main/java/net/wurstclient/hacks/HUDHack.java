e2b9227087389a7ad7deb5211967938a9675cf59
Tyler <22119302+novo52@users.noreply.github.com>
2020-05-29 22:12:33 -0600

    Changed InfoHUD so it can be swapped to the right side.

--- src/main/java/net/wurstclient/hacks/HUDHack.java
+++ src/main/java/net/wurstclient/hacks/HUDHack.java
@@ -4,21 +4,31 @@ import net.wurstclient.Category;
 import net.wurstclient.SearchTags;
 import net.wurstclient.hack.Hack;
 import net.wurstclient.settings.CheckboxSetting;
+import net.wurstclient.settings.EnumSetting;
 
 @SearchTags({"HUD"})
-public final class HUDHack extends Hack 
+public final class HUDHack extends Hack
 {
-	private final CheckboxSetting fps = new CheckboxSetting("FPS","Display your current FPS", true);
-	private final CheckboxSetting speed = new CheckboxSetting("Speed","Display your current speed", true);
-	private final CheckboxSetting coords = new CheckboxSetting("Coordinates","Display your current coordinates", true);
-	private final CheckboxSetting dim_coord = new CheckboxSetting("Dimension Coordinates","Display your Nether coordinates in the Owerworld or your Owerworld coordinates in the Nether", true);
-
-	public HUDHack() {
+	private final EnumSetting position = new EnumSetting("Position", "Where to render the HUD", Position.values(), Position.LEFT);
+	private final CheckboxSetting fps = new CheckboxSetting("FPS", "Display your current FPS", true);
+	private final CheckboxSetting speed = new CheckboxSetting("Speed", "Display your current speed", true);
+	private final CheckboxSetting coords = new CheckboxSetting("Coordinates", "Display your current coordinates", true);
+	private final CheckboxSetting dim_coord = new CheckboxSetting("Dimension Coordinates", "Display your Nether coordinates in the Owerworld or your Owerworld coordinates in the Nether", true);
+	
+	public HUDHack()
+	{
 		super("HUD", "Display useful information on your screen");
 		setCategory(Category.OTHER);
+		addSetting(position);
 		addSetting(fps);
 		addSetting(speed);
 		addSetting(coords);
 		addSetting(dim_coord);
+	}
+	
+	public enum Position
+	{
+		LEFT,
+		RIGHT
 	}
 }
--- src/main/java/net/wurstclient/hud/InfoHUD.java
+++ src/main/java/net/wurstclient/hud/InfoHUD.java
@@ -1,98 +1,117 @@
 package net.wurstclient.hud;
 
-import java.util.Map;
-
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.util.math.Position;
 import net.minecraft.world.dimension.DimensionType;
 import net.wurstclient.WurstClient;
+import net.wurstclient.hacks.HUDHack;
+import net.wurstclient.other_features.HackListOtf;
 import net.wurstclient.settings.CheckboxSetting;
+import net.wurstclient.settings.EnumSetting;
 import net.wurstclient.settings.Setting;
 
-public class InfoHUD {
+import java.util.Map;
+
+public class InfoHUD
+{
 	private double speed;
 	private Position prevPos;
 	private long prevTime;
-	private Map<String,Setting> settings;
-	public InfoHUD() {
+	private Map<String, Setting> settings;
+	
+	public InfoHUD()
+	{
 		settings = WurstClient.INSTANCE.getHax().hudHack.getSettings();
 		prevTime = System.currentTimeMillis();
 		speed = 0;
-		prevPos = new Position(){
-		
+		prevPos = new Position()
+		{
+			
 			@Override
-			public double getZ() {
+			public double getX()
+			{
 				return 0;
 			}
-		
+			
 			@Override
-			public double getY() {
+			public double getY()
+			{
 				return 0;
 			}
-		
+			
 			@Override
-			public double getX() {
+			public double getZ()
+			{
 				return 0;
 			}
 		};
 	}
-	public void renderer(float partialTicks) {
-		int screenHeight = WurstClient.MC.getWindow().getScaledHeight();
-		int xPos = 2;
-		int yOffset = 10;
-		int yPos = screenHeight-yOffset;
-
+	
+	public void renderer(float partialTicks)
+	{
 		Position pos = WurstClient.MC.player.getPos();
 		
-		if(((CheckboxSetting)settings.get("dimension coordinates")).isChecked()){
-			int color;
-			String dimension_pos;
-			if(WurstClient.MC.player.dimension == DimensionType.THE_NETHER) {
-				color = 0xffffffff;
-				dimension_pos = "X: " + (int)Math.floor(pos.getX()*8) + " Y: " + (int)Math.floor(pos.getY()) + " Z: " + (int)Math.floor(pos.getZ()*8); 
-			}
-			else {
-				color = 0xffff0000;
-				dimension_pos = "X: " + (int)Math.floor(pos.getX()/8) + " Y: " + (int)Math.floor(pos.getY()) + " Z: " + (int)Math.floor(pos.getZ()/8); 
-			}
-			drawText(dimension_pos, xPos, yPos, color);
-			yPos-=yOffset;
+		int currentLayer = 1;
+		
+		boolean inNether = WurstClient.MC.player.dimension == DimensionType.THE_NETHER;
+		
+		int white = 0xffffffff;
+		int red = 0xffff0000;
+		int dimensionColor = inNether ? white : red;
+		int normalColor = 	!inNether ? white : red;
+		
+		if(((CheckboxSetting) settings.get("dimension coordinates")).isChecked())
+		{
+			float multiplier = inNether ? 8 : 1.25F;
+			String dimension_pos = String.format("X: %d, Y: %d, Z: %d", (int) (pos.getX() * multiplier), (int) (pos.getY()), (int) (pos.getZ() * multiplier));
+			drawText(dimension_pos, currentLayer++, dimensionColor);
 		}
-
-		if(((CheckboxSetting)settings.get("coordinates")).isChecked()){
-			String defpos = "X: " + (int)Math.floor(pos.getX()) + " Y: " + (int)Math.floor(pos.getY()) + " Z: " + (int)Math.floor(pos.getZ()); 
-			int color;
-			if(WurstClient.MC.player.dimension == DimensionType.THE_NETHER) {
-				color = 0xffff0000;
-			}
-			else {
-				color = 0xffffffff;
-			}
-			drawText(defpos, xPos, yPos, color);
-			yPos -= yOffset;
+		
+		if(((CheckboxSetting) settings.get("coordinates")).isChecked())
+		{
+			String defpos = String.format("X: %d, Y: %d, Z: %d", (int) pos.getX(), (int) pos.getY(), (int) pos.getZ());
+			drawText(defpos, currentLayer++, normalColor);
 		}
-		//TODO: Calculate the speed more accurately
-		if(((CheckboxSetting)settings.get("speed")).isChecked()){
+		// TODO: Calculate the speed more accurately
+		if(((CheckboxSetting) settings.get("speed")).isChecked())
+		{
 			long currTime = System.currentTimeMillis();
-			long deltaTime = currTime-prevTime;
-			if(deltaTime > 500){
-				double dist = Math.sqrt(Math.pow(Math.abs(pos.getX()-prevPos.getX()),2)+Math.pow(Math.abs(pos.getZ()-prevPos.getZ()),2));
-				speed = (double)dist/((double)deltaTime/1000);
+			long deltaTime = currTime - prevTime;
+			if(deltaTime > 500)
+			{
+				double dist = Math.sqrt(Math.pow(pos.getX() - prevPos.getX(), 2) + Math.pow(pos.getZ() - prevPos.getZ(), 2));
+				speed = dist / ((double) deltaTime / 1000);
 				prevPos = pos;
 				prevTime = currTime;
 			}
-			drawText(Math.round(speed*10.0)/10.0 + " m/s", xPos, yPos, 0xffffffff);
-			yPos-=yOffset;
+			drawText(Math.round(speed * 10.0) / 10.0 + " m/s", currentLayer++, 0xffffffff);
 		}
-		if(((CheckboxSetting)settings.get("fps")).isChecked()) {
-			drawText(WurstClient.MC.fpsDebugString.split(" fps")[0]+" FPS", xPos, yPos, 0xffffffff);
-			yPos -= yOffset;
-		}
+		if(((CheckboxSetting) settings.get("fps")).isChecked())
+			drawText(WurstClient.MC.fpsDebugString.split(" fps")[0] + " FPS", currentLayer, 0xffffffff);
 	}
-
-	public void drawText(String s, int x, int y, int color) {
+	
+	public void drawText(String s, int layer, int color)
+	{
 		TextRenderer tr = WurstClient.MC.textRenderer;
-
-		tr.draw(s, x, y, color);
+		
+		int posX;
+		int posY;
+		
+		int screenWidth = WurstClient.MC.getWindow().getScaledWidth();
+		int screenHeight = WurstClient.MC.getWindow().getScaledHeight();
+		
+		int layerOffset = 10 * layer;
+		posY = screenHeight - layerOffset - 2;
+		
+		EnumSetting hudPosition = (EnumSetting) settings.get("position");
+		if(hudPosition.getSelected() == HUDHack.Position.LEFT)
+			posX = 2;
+		else
+		{
+			int stringWidth = tr.getStringWidth(s);
+			posX = screenWidth - stringWidth - 2;
+		}
+		
+		tr.draw(s, posX, posY, color);
 	}
 }
